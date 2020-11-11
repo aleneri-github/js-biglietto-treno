4c0c095dc91d33b66fac5be03f64299eaaa81545
Alessandro <aleneri242@gmail.com>
2020-11-11 14:03:47 +0100

    Creazione cartella e debug

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// NOTE: Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km);
+// va applicato uno sconto del 20% per i minorenni;
+// va applicato uno sconto del 40% per gli over 65.
+
+
+// richiesta km da percorrere
+
+// richiesta età passeggero
+
+// prezzo del Biglietto
+
+// sconto 20% minorenni
+
+// sconto 40% over 65
