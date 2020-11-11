8791080ffbbf3259af385b14504c259dcea32916
Alessandro <aleneri242@gmail.com>
2020-11-11 16:19:52 +0100

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -3,29 +3,40 @@
 // il prezzo del biglietto è definito in base ai km (0.21 € al km);
 // va applicato uno sconto del 20% per i minorenni;
 // va applicato uno sconto del 40% per gli over 65.
+var prezzoAlKm = 0.21;
 
 var richiestaKm = prompt("Inserisci i km che vuoi percorrere");
 
-var aNNi = prompt("Inserisci la tua età");
 
-var minOMagg = "";
-if (aNNi < 18) {
-  minOMagg = "minorenne";
-} else {
-  minOMagg = "maggiorenne";
-}
-console.log(minOMagg)
-
-var prezzoAlKm = 0.21;
+var anni = prompt("Inserisci la tua età");
 
 var prezzoIntero = richiestaKm * prezzoAlKm;
 console.log(prezzoIntero);
 
 
+var sconto = 0;
 
-function percentage(num, per)
-{
-  return (num/100)*per;
+if (anni < 18) {
+  sconto = (prezzoIntero * 20 ) / 100;
+
+} else if (anni > 65) {
+  sconto = (prezzoIntero * 40) / 100;
 }
+
+var prezzoFinale = prezzoIntero - sconto;
+console.log(prezzoFinale);
+
+
+
+
+
+
+console.log(minOMagg)
+
+
+
+
+
+
 
 console.log(percentage(1000, 47.12));
